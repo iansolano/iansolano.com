1f98c510647443025bbf51ec44e06625ddac0bf3
Ian Solano-Kamaiko <2371418+iansolano@users.noreply.github.com>
2021-01-07 14:37:44 -0500

    add style-components and refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
-import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-const App = ({ Component, pageProps }: AppProps) =>
-  <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  </ThemeProvider>
+)
 
 export default App;
+
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+`;
